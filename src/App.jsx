90a95b24f4abdeee7ba3601bd492b01e2214e5de
nsptel <nilkumarpatel48@gmail.com>
2022-05-08 12:04:22 -0400

    [WIP] Get multiple strings working
    Neel's code is commented. Chintan's solution is working properly with
    some console errors here and there.

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,22 @@
-import './App.css';
-import DevPosition from './Components/DevPosition';
+import { useState } from "react";
+import "./App.css";
+import DevPosition from "./Components/DevPosition";
 
 function App() {
+  const devStrings = [
+    "Front End Developer",
+    "Back End Developer",
+    "Full Stack Developer",
+  ];
+  const [stringInd, setStringInd] = useState(0);
+
   return (
     <div className="App">
       <div className="position">
-        <DevPosition />
+        <DevPosition
+          devString={devStrings[stringInd]}
+          updatePosition={setStringInd}
+        />
       </div>
     </div>
   );
--- src/Components/DevPosition.jsx
+++ src/Components/DevPosition.jsx
@@ -1,27 +1,54 @@
 import { useEffect, useState } from "react";
 import "../App.css";
 
-export default function DevPosition() {
-  const devString = "Full Stack Developer ";
-  const [directionRight, setDirectionRight] = useState(1);
+export default function DevPosition({ devString, updatePosition }) {
   const [currentString, setCurrentString] = useState("");
+  //   const [ind, setInd] = useState(null);
+  const [didRun, setDidRun] = useState(true);
+  const [reachEnd, setReachEnd] = useState(false);
 
   const updateDevString = () => {
-    setTimeout(() => {
-      setCurrentString((prevState) => {
-        if (prevState.length >= devString.length - 1) {
-          setDirectionRight(-1);
-        } else if (prevState.length <= 1) {
-          setDirectionRight(1);
-        }
-        return devString.slice(0, prevState.length + directionRight);
-      });
-    }, 200);
+    if (didRun) {
+      setDidRun(false);
+    } else {
+      setTimeout(() => {
+        setCurrentString((prevState) => {
+          if (reachEnd && prevState.length <= 1) {
+            console.log(currentString);
+            updatePosition((curr) => (curr + 1) % 3);
+            setDidRun(false);
+            setReachEnd(false);
+            return "";
+          } else if (prevState.length >= devString.length || reachEnd) {
+            setReachEnd(true);
+            return devString.slice(0, prevState.length - 1);
+          } else return devString.slice(0, prevState.length + 1);
+        });
+      }, 100);
+    }
+    // setTimeout(() => {
+    //   setCurrentString(() =>
+    //     devString.slice(0, devString.length - Math.abs(ind))
+    //   );
+    //   setInd((prevInd) => prevInd - 1);
+    // }, 200);
   };
 
   useEffect(() => {
     updateDevString();
-  }, [currentString]);
+  }, [currentString, didRun]);
+
+  //   useEffect(() => {
+  //     setInd(devString.length);
+  //   }, [devString]);
+
+  //   useEffect(() => {
+  //     if (ind === -devString.length) {
+  //       setInd(null);
+  //       updatePosition((prevPos) => (prevPos >= 2 ? 0 : prevPos + 1));
+  //     }
+  //     updateDevString();
+  //   }, [currentString]);
 
   return (
     <div className="position-container">
